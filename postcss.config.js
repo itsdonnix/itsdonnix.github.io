5e4dce7c8277153a8fdf580644bda1be2bec0f50
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-02-09 14:02:53 +0800

    Fix purgecss purgin some content

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,19 @@
 const PRODUCTION = process.env.HUGO_ENVIRONMENT === "production";
 
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./hugo_stats.json"],
+  defaultExtractor: (content) => {
+    let els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids);
+  },
+});
+
 module.exports = {
   syntax: "postcss-scss",
   plugins: [
     require("tailwindcss"),
-    PRODUCTION &&
-      require("@fullhuman/postcss-purgecss")({
-        content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
-      }),
     PRODUCTION && require("autoprefixer"),
+    ...(PRODUCTION ? [purgecss] : []),
     // PRODUCTION && require("cssnano")(),
   ],
 };
